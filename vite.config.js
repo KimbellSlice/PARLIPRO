b0865682d3885cf43cd321594cdc32f76c9fbb0d
KimbellSlice <ckimbell@gmail.com>
2026-04-29 16:45:52 -0500

    Added prerendering

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { htmlPrerender } from 'vite-plugin-html-prerender'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    htmlPrerender({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: ['/'],
+    })
+  ],
 })
